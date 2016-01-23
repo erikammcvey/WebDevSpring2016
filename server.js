f2007838cee7fe8e464561d08f69ae7810a0c5d0
Erika McVey <Erika McVey>
2016-01-23 09:44:34 -0500

    working on hackathon stuffs and hoping it works

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
+app.post('/populatedata', function(req, res) {
+    res.send('cats, dogs, animals!');
+});
 app.listen(port, ipaddress);
