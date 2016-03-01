1174e6b2ba2298ee3754f5fd9538d5849c1971e6
Erika McVey <Erika McVey>
2016-03-01 00:14:03 -0500

    add profile controller

---
+++ public/assignment/views/users/profile.controller.js
@@ -0,0 +1,15 @@
+(function(){
+    angular
+        .module("FormMakerApp")
+        .controller("ProfileController", ProfileController);
+
+    function ProfileController($location, $scope, $rootScope, UserService) {
+        $scope.update = update;
+
+        function update(user) {
+            UserService.updateUser(user._id, user, function(res){
+                $rootScope.currentUser = user;
+            });
+        }
+    }
+});
