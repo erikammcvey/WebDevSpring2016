7c571f16a59d320228b2320e6d2c8132151fc640
Erika McVey <Erika McVey>
2016-03-01 00:38:58 -0500

    login controller

---
+++ public/assignment/views/users/login.controller.js
@@ -0,0 +1,18 @@
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("LoginController", LoginController);
+
+    function LoginController($scope, $rootScope, $location, UserServices) {
+        $scope.login = login;
+
+        function login(user) {
+            UserServices.findUserByCredentials(user.username, user.password, function(res){
+                if(res) {
+                    $rootScope.currentUser = res;
+                    $location.url("/profile");
+                }
+            });
+        }
+    }
+});
