56b4e5815d0d08f281a964d71d51b7ba7122ac3d
Erika McVey <Erika McVey>
2016-03-18 20:22:17 -0400

    add form model

---
+++ public/assignment/server/models/form.model.js
@@ -0,0 +1,59 @@
+var mock = require("./form.mock.json");
+var uuid = require("node-uuid");
+
+module.exports = function(app) {
+    var api = {
+        createForm: createForm,
+        findAllForms: findAllForms,
+        findFormById: findFormById,
+        updateForm: updateForm,
+        deleteFormById: deleteFormById,
+        findFormByTitle: findFormByTitle
+    };
+    return api;
+
+    var forms = findAllForms();
+
+    function createForm(form) {
+        form._id = (new Date()).getTime();
+        mock.push(form);
+        return mock;
+    }
+
+    function findAllForms() {
+        return mock;
+    }
+
+    function findFormById(formId) {
+        for (var i = 0; i < forms.length; i++) {
+            if (forms[i] === formId) {
+                return forms[i];
+            }
+        }
+    }
+
+    function updateForm(formId, newForm) {
+        for (var i = 0; i < forms.length(); i++) {
+            var form = forms[i];
+            if (form._id === formId) {
+                forms[i] = newForm;
+            }
+        }
+    }
+
+    function deleteFormById(formId) {
+        var form = findFormById(formId);
+        if (form) {
+            var index = forms.indexOf(form);
+            forms.splice(index, 1);
+        }
+    }
+
+    function findFormByTitle(title) {
+        for (var i = 0; i < forms.length; i++) {
+            if (forms[i] === title) {
+                return forms[i];
+            }
+        }
+    }
+};
