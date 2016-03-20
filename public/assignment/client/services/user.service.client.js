19235f2968c50af96ce50950fe87a41bef3536cb
Erika McVey <Erika McVey>
2016-03-20 14:09:00 -0400

    refactored user client service

--- public/assignment/client/services/user.service.client.js
+++ public/assignment/client/services/user.service.client.js
@@ -4,70 +4,65 @@
         .module("FormBuilderApp")
         .factory("UserService", UserService);
 
-    function UserService(){
-        var users = [
-            {        "_id":123, "firstName":"Alice",            "lastName":"Wonderland",
-                "username":"alice",  "password":"alice",   "roles": ["student"]                },
-            {        "_id":234, "firstName":"Bob",              "lastName":"Hope",
-                "username":"bob",    "password":"bob",     "roles": ["admin"]                },
-            {        "_id":345, "firstName":"Charlie",          "lastName":"Brown",
-                "username":"charlie","password":"charlie", "roles": ["faculty"]                },
-            {        "_id":456, "firstName":"Dan",              "lastName":"Craig",
-                "username":"dan",    "password":"dan",     "roles": ["faculty", "admin"]},
-            {        "_id":567, "firstName":"Edward",           "lastName":"Norton",
-                "username":"ed",     "password":"ed",      "roles": ["student"]                }
-        ];
-        var services = {
-            users: users,
+    function UserService($http, $rootScope){
+        var api = {
             findUserByCredentials: findUserByCredentials,
+            findUserByUsername: findUserByUsername,
             findAllUsers: findAllUsers,
             createUser: createUser,
             deleteUserById: deleteUserById,
             updateUser: updateUser
         };
-        return services;
+        return api;
 
-        function findUserByCredentials(username, password, callback) {
-            for (var u = 0; u < services.users.length; u++) {
-                var user = services.users[u];
-                if (user.username === username &&
-                    user.password === password) {
-
-                    callback(user);
-                    break;
+        function findUserByCredentials(username, password) {
+            return $http({
+                method: 'GET',
+                url: 'api/assignment/user',
+                params: {
+                    username: username,
+                    password: password
                 }
-            }
-            callback(null);
+            });
         }
 
-        function findAllUsers(callback) {
-            callback(services.users);
+        function findUserByUsername(username) {
+            return $http({
+                method: 'GET',
+                url: 'api/assignment/user',
+                params: {username: username}
+            });
         }
 
-        function createUser(user, callback) {
-            user["_id"] = (new Date()).getTime();
-            services.users.push(user);
-            callback(user);
+        function findAllUsers() {
+            return $http({
+                method: 'GET',
+                url: 'api/assignment/user'
+            });
         }
 
-        function deleteUserById(userId, callback) {
-            for (var u = 0; u < services.users.length; u++) {
-                var user = services.users[u];
-                if (user._id === userId) {
-                    services.users.splice(u, 1);
-                }
-            }
-            callback(services.users)
+        function createUser(user) {
+            return $http({
+                method: 'POST',
+                url: 'api/assignment/user',
+                data: user
+            });
+        }
+
+        function deleteUserById(userId) {
+            return $http({
+                method: 'DELETE',
+                url: 'api/assignment/user',
+                params: {userId: userId}
+            });
         }
 
         function updateUser(userId, user, callback) {
-            for (var u = 0; u < services.users.length; u++) {
-                var curUser = services.users[u];
-                if (curUser._id === userId) {
-                    services.users[u] = user;
-                    callback(curUser);
-                }
-            }
+            return $http({
+                method: 'PUT',
+                url: 'api/assignment/user',
+                data: user
+            });
         }
     }
 })();
