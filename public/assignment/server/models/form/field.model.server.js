5701844628f48128c85f278ba2b4409062730d09
Erika McVey <Erika McVey>
2016-04-04 17:43:37 -0400

    field model, trying to get mongoose to work still, it is undefined?

---
+++ public/assignment/server/models/form/field.model.server.js
@@ -0,0 +1,83 @@
+var uuid = require("node-uuid");
+var q = require("q");
+
+module.exports = function(db, mongoose) {
+    var FieldSchema = require("./field.schema.server.js")(mongoose);
+    var FieldModel = mongoose.model('FieldModel', FieldSchema);
+
+    var api = {
+        getFieldById: getFieldById,
+        createField: createField,
+        updateField: updateField,
+        deleteField: deleteField
+    };
+    return api;
+
+    function getFieldById(fieldId) {
+        var deferred = q.defer();
+        FieldModel.findById(
+            fieldId,
+            function (error, doc) {
+                if(error) {
+                    deferred.reject(error);
+                } else {
+                    deferred.resolve(doc);
+                }
+            }
+        );
+    }
+
+    function createField(field) {
+        var deferred = q.defer();
+        FieldModel.create(
+            field,
+            function(error, doc) {
+                if(error) {
+                    deferred.reject(error);
+                }
+                else {
+                    deferred.resolve(doc);
+                }
+            });
+        return deferred.promise;
+    }
+
+    function deleteField(fieldId) {
+        var deferred = q.defer();
+
+        FieldModel.remove(
+            {_id:fieldId},
+            function (error, doc) {
+                if(error) {
+                    deferred.reject(error);
+                } else {
+                    deferred.resolve(doc);
+                }
+            }
+        );
+        return deferred.promise;
+    }
+
+    function updateField(fieldId, newField) {
+        var deferred = q.defer();
+        FieldModel.update(
+            {_id: fieldId},
+            {$set: newField},
+            function (error, doc) {
+                if(error) {
+                    deferred.reject(error);
+                } else {
+                    FieldModel.findById(fieldId,
+                        function(error, doc) {
+                            if(error) {
+                                deferred.reject(error);
+                            }
+                            else {
+                                deferred.resolve(doc);
+                            }
+                        });
+                }
+            });
+        return deferred.promise;
+    }
+};
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var app = express();
-var bodyParser    = require('body-parser');
+var bodyParser = require('body-parser');
 var multer = require('multer');
+var mongoose = require('mongoose');
+var connectionString = 'mongodb://localhost/WebDev';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
@@ -11,10 +23,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
 app.use(express.static(__dirname + '/public'));
 
-require("./public/assignment/server/app.js")(app);
-require("./public/assignment/server/services/user.service.server.js")(app);
-require("./public/assignment/server/services/form.service.server.js")(app);
-require("./public/assignment/server/services/field.service.server.js")(app);
+require("./public/assignment/server/app.js")(app, db, mongoose);
+//require("./public/assignment/server/services/user.service.server.js")(app);
+//require("./public/assignment/server/services/form.service.server.js")(app);
+//require("./public/assignment/server/services/field.service.server.js")(app);
 
 require("./public/project/server/app.js")(app);
 require("./public/project/server/services/user.service.server.js")(app);
